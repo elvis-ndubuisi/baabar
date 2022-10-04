2b74d01cfdb0b2f7aef237986848add284631b5a
elvis-ndubuisi <provictor.ie@gmail.com>
2022-10-04 12:36:23 +0100

    new

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,54 @@
 import Link from "next/link";
+import { FiLinkedin, FiTwitter, FiMail, FiCoffee } from "react-icons/fi";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <section className="wrapper">
-        <Link href="/">
-          <div className="brand">BaaBar.</div>
-        </Link>
+        <div>
+          <Link href="/">
+            <div className="brand">BaaBar.</div>
+          </Link>
+        </div>
 
         <nav>
+          <h3>Quick Links</h3>
           <ul>
             <li>
-              <Link href="shortener">shortener</Link>
+              <Link href="shortener">Shorten Link</Link>
             </li>
             <li>
-              <Link href="/qrcode">qr code</Link>
+              <Link href="/qrcode">QR code</Link>
+            </li>
+            <li>
+              <Link href="/qrcode">Scan QR code</Link>
             </li>
           </ul>
         </nav>
 
+        <section className="misc">
+          <h3>Other Services</h3>
+          <a href="">Lorem Generator</a>
+          <a href="">Lorem Generator</a>
+          <a href="">Lorem Generator</a>
+        </section>
+
         <section className="socials">
-          <a href="">Li</a>
-          <a href="">gm</a>
-          <a href="">tw</a>
+          <div>
+            <a href="">
+              <FiLinkedin />
+            </a>
+            <a href="">
+              <FiTwitter />
+            </a>
+            <a href="">
+              <FiMail />
+            </a>
+          </div>
+          <button>
+            <FiCoffee size={24} />
+            <span>Send a Tip</span>
+          </button>
         </section>
       </section>
     </footer>
